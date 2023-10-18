3a8ad11bbdd460bf0f2b946f8b42e66fc3216c9e
Atiq Mansoori <er_asm@outlook.com>
2023-10-18 16:26:39 +0530

    Project structure

---
+++ src/best_optimal_algo/__init__.py
@@ -0,0 +1,13 @@
+
+class BestOptimalAlgo:
+
+    def __init__(self) -> None:
+        pass
+
+    models={}
+
+    parameters={}
+
+    def best_optimal_algo( X_train, X_test, y_train, y_test):
+
+        pass
